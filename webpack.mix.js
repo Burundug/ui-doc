c40b4c121e2a7a3021137933fb22ce86f74e02f9
Burundug <iburundugi@gmail.com>
2025-05-03 15:29:52 +0300

    add config.php

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ require('laravel-mix-jigsaw');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build'); // или просто 'build' если хочешь стандартную структуру
-mix.copy('source/_core/_assets/img', 'source/assets/build');
+mix.copy('source/_core/_assets/img', 'source/assets/build/img');
 mix.jigsaw()
     .js('source/_core/_assets/js/main.js', 'js')
     .css('source/_core/_assets/css/main.css', 'css', [ // изменил путь с build/css на css
